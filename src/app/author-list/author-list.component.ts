aa47f5a862f262aca775ed948122d65eb196fdd1
Ruowen0613 <133537454+Ruowen0613@users.noreply.github.com>
2024-07-11 13:13:36 -0400

    add angular material to author-list

--- src/app/author-list/author-list.component.ts
+++ src/app/author-list/author-list.component.ts
@@ -4,17 +4,31 @@ import { AuthorsService } from '../author.service';
 import { Router } from '@angular/router';
 import {RouterModule} from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-author-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [
+      CommonModule, 
+      RouterModule, 
+      MatIconModule, 
+      MatTableModule, 
+      MatButtonModule, 
+      MatToolbarModule
+  ],
   templateUrl: './author-list.component.html',
   styleUrl: './author-list.component.css'
 })
 
 export class AuthorListComponent implements OnInit {
-  authors: any[] = [];
+  authors = new MatTableDataSource<any>();  // Use MatTableDataSource for Angular Material Table
+  displayedColumns: string[] = [
+    'au_id', 'au_lname', 'au_fname', 'phone', 'address', 
+    'city', 'state', 'zip', 'contract', 'actions'
+  ];
 
   constructor(private authorsService: AuthorsService, private router: Router) {}
 
@@ -26,7 +40,7 @@ export class AuthorListComponent implements OnInit {
     this.authorsService.getAuthors()
       .subscribe(
         (data) => {
-          this.authors = data;
+          this.authors.data = data;
         },
         (error) => {
           console.error('Error fetching authors:', error);
@@ -47,7 +61,7 @@ export class AuthorListComponent implements OnInit {
     if (confirm('Are you sure you want to delete this author?')) {
       this.authorsService.deleteAuthor(au_id).subscribe(
         () => {
-          this.authors = this.authors.filter(author => author.au_id !== au_id);
+          this.authors.data = this.authors.data.filter((author: any) => author.au_id !== au_id);
           alert('Author deleted successfully');
         },
         error => {
